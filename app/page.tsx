2a57fe20286d72613ea2eb1b2ee89e48f3fb9537
React-Redux <abdulmugees44@gmail.com>
2023-08-16 17:59:24 +0500

    set name

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import React from 'react';
-import WeatherApp from '../components/Test'; 
+import WeatherApp from '../components/WeatherApp'; 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
